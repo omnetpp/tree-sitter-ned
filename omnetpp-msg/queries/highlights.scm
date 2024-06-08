dba3d746153b207f4059e52fbf310dd4e15148c3
Gyorgy Szaszko <adam.szaszko@gmail.com>
2024-06-08 14:01:09 +0200

    added highlights.scm

---
+++ omnetpp-msg/queries/highlights.scm
@@ -0,0 +1,9 @@
+; highlights.scm
+
+; "class" @keyword
+; "return" @keyword
+; (type_identifier) @type
+; (int_literal) @number
+; (function_declaration name: (identifier) @function)
+
+(class) @keyword
