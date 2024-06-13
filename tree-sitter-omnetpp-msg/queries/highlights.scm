ba0cc63f17a902dcc10a115501072b8c4165cdce
Gyorgy Szaszko <adam.szaszko@gmail.com>
2024-06-13 15:45:45 +0200

    x

--- tree-sitter-omnetpp-msg/queries/highlights.scm
+++ tree-sitter-omnetpp-msg/queries/highlights.scm
@@ -6,4 +6,16 @@
 ; (int_literal) @number
 ; (function_declaration name: (identifier) @function)
 
-(class) @keyword
+"class" @keyword
+"packet" @keyword
+"import" @keyword
+"struct" @keyword
+"enum" @keyword
+"extends" @keyword
+"cplusplus" @keyword
+(comment) @comment
+(inline_comment) @comment
+(importspec) @comment
+(data_type) @type
+(_targetitem) @variable.parameter
+(prop_name) @prop
